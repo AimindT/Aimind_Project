5b029dcb7290e7a003d229283a9de459dea03bbe
Chris <L21170291@culiacan.tecnm.mx>
2025-02-21 21:35:42 -0700

    Update

--- aimind/lib/main.dart
+++ aimind/lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:aimind/screens/Auth_Screens/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
+
+  String supabaseUrl = dotenv.env['SUPABASE_URL'] ?? '';
+  String supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY'] ?? '';
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseAnonKey,
+  );
+
   runApp(MainApp());
 }
 
