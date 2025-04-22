14c89d6fec6ed8e383fcb15a9d4322637e8444ed
Roberto Carlos <saucedorodriguezrobertocarlos@gmail.com>
2025-04-21 22:30:43 -0700

    archivo agregado

---
+++ aimind/lib/screens/functionalities_Screens/chat_bot/mainChat.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Mainchat extends StatefulWidget {
+  const Mainchat({super.key});
+  @override
+  State<Mainchat> createState() => _MainchatState();
+}
+
+class _MainchatState extends State<Mainchat> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
